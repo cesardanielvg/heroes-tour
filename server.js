6b9657448e8bb38825f3e6bb97baeb68977bde3c
cesar <cesar.vergara.guerrero@alumnos.udg.mx>
2020-11-20 22:42:50 -0600

    Fix package.json to include heroku tricks

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 
-const express = require('express');
 
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('./dist/angular-tour-of-heroes'));
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/angular-tour-of-heroes/'}
-);
-});
+app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+
+    '/dist/angular-tour-of-heroes/index.html'));});
 app.listen(process.env.PORT || 8080);
+
+
+
+console.log('listening')
